9d170745052933511384f1c56fcaea9ff93dfa81
Henryk Konsek <hekonsek@gmail.com>
2016-11-25 09:06:34 +0100

    Mongo port should be configurable.

--- lib/mongo/src/main/groovy/net/streamok/lib/mongo/EmbeddedMongo.groovy
+++ lib/mongo/src/main/groovy/net/streamok/lib/mongo/EmbeddedMongo.groovy
@@ -1,21 +1,39 @@
+/**
+ * Licensed to the Streamok under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.streamok.lib.mongo
 
 import de.flapdoodle.embed.mongo.MongodStarter
-import de.flapdoodle.embed.mongo.config.IMongodConfig
 import de.flapdoodle.embed.mongo.config.MongodConfigBuilder
 import de.flapdoodle.embed.mongo.config.Net
-import de.flapdoodle.embed.mongo.distribution.Version
-import de.flapdoodle.embed.process.runtime.Network
+
+import static de.flapdoodle.embed.mongo.distribution.Version.Main.V3_2
+import static de.flapdoodle.embed.process.runtime.Network.localhostIsIPv6
 
 class EmbeddedMongo {
 
-    void start() {
-        IMongodConfig mongodConfig = new MongodConfigBuilder()
-                .version(Version.Main.PRODUCTION)
-                .net(new Net(27017, Network.localhostIsIPv6()))
-                .build();
+    void start(int port) {
+        def config = new MongodConfigBuilder()
+                .version(V3_2).net(new Net(port, localhostIsIPv6()))
+                .build()
+        MongodStarter.getDefaultInstance().prepare(config).start()
+    }
 
-        MongodStarter.getDefaultInstance().prepare(mongodConfig).start()
+    void start() {
+        start(27017)
     }
 
 }
