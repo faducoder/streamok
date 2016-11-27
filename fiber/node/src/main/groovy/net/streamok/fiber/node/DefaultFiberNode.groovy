9f282f3a032781df7e182fd37fa91fe644211c53
Henryk Konsek <hekonsek@gmail.com>
2016-11-27 22:18:47 +0100

    Fiber node should use /tmp as Vert.x cache directory.

--- fiber/node/src/main/groovy/net/streamok/fiber/node/DefaultFiberNode.groovy
+++ fiber/node/src/main/groovy/net/streamok/fiber/node/DefaultFiberNode.groovy
@@ -1,27 +1,39 @@
+/**
+ * Licensed to the Streamok under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.streamok.fiber.node
 
 import io.vertx.core.Vertx
-import net.streamok.fiber.node.api.DependencyProvider
-import net.streamok.fiber.node.api.Endpoint
-import net.streamok.fiber.node.api.FiberContext
-import net.streamok.fiber.node.api.FiberDefinition
-import net.streamok.fiber.node.api.FiberNode
-import net.streamok.fiber.node.api.FiberNodeAware
-import net.streamok.fiber.node.api.FiberSuite
+import net.streamok.fiber.node.api.*
+
+import static org.apache.commons.lang3.SystemUtils.javaIoTmpDir
 
 class DefaultFiberNode implements FiberNode {
 
-    private final def vertx = Vertx.vertx()
+    private final def Vertx vertx
 
     def dependencies = [:]
 
-    DefaultFiberNode addFiber(FiberDefinition fiberDefinition) {
-        vertx.eventBus().consumer(fiberDefinition.address()){fiberDefinition.handler().handle(new FiberContext(it, this))}
-        this
+    DefaultFiberNode() {
+        System.setProperty('vertx.cacheDirBase', javaIoTmpDir.absolutePath)
+        vertx = Vertx.vertx()
     }
 
-    DefaultFiberNode addDependency(DependencyProvider dependencyProvider) {
-        dependencies[dependencyProvider.key()] = dependencyProvider.dependency()
+    DefaultFiberNode addFiber(FiberDefinition fiberDefinition) {
+        vertx.eventBus().consumer(fiberDefinition.address()){fiberDefinition.handler().handle(new FiberContext(it, this))}
         this
     }
 
@@ -39,6 +51,13 @@ class DefaultFiberNode implements FiberNode {
         this
     }
 
+    // Dependency injection
+
+    DefaultFiberNode addDependency(DependencyProvider dependencyProvider) {
+        dependencies[dependencyProvider.key()] = dependencyProvider.dependency()
+        this
+    }
+
     Object dependency(String key) {
         dependencies[key]
     }
@@ -46,6 +65,8 @@ class DefaultFiberNode implements FiberNode {
     def <T> T dependency(Class<T> type) {
         dependencies.values().find { type.isAssignableFrom(it.getClass()) }
     }
+
+    // Getters
 
     Vertx vertx() {
         vertx
