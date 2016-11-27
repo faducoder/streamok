2809a0e2549762f639d2b161e287dda33ace7cc4
Henryk Konsek <hekonsek@gmail.com>
2016-11-27 22:07:57 +0100

    Install script should install Docker.

--- distribution/install.sh
+++ distribution/install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Install Docker
+curl -sSL https://get.docker.com/ | sh && sudo systemctl enable docker.service && sudo systemctl start docker
+
 wget -nc https://github.com/openshift/origin/releases/download/v1.4.0-rc1/openshift-origin-server-v1.4.0-rc1.b4e0954-linux-64bit.tar.gz
 tar xpf openshift-origin-server-v1.4.0-rc1.b4e0954-linux-64bit.tar.gz
 mv openshift-origin-server-v1.4.0-rc1+b4e0954-linux-64bit openshift
