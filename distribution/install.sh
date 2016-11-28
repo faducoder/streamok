05eaf11b9f0b7ef5f4fec158f8a3c003b8a2512a
Henryk Konsek <hekonsek@gmail.com>
2016-11-28 17:42:13 +0100

    Improved installation script conditions.

--- distribution/install.sh
+++ distribution/install.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 # Install Docker
-curl -sSL https://get.docker.com/ | sh && sudo systemctl enable docker.service && sudo systemctl start docker
+which docker > /dev/null || (curl -sSL https://get.docker.com/ | sh && sudo systemctl enable docker.service && sudo systemctl start docker)
 
 wget -nc https://github.com/openshift/origin/releases/download/v1.4.0-rc1/openshift-origin-server-v1.4.0-rc1.b4e0954-linux-64bit.tar.gz
-tar xpf openshift-origin-server-v1.4.0-rc1.b4e0954-linux-64bit.tar.gz
-mv openshift-origin-server-v1.4.0-rc1+b4e0954-linux-64bit openshift
+if [ ! -d 'openshift' ]; then
+    tar xpf openshift-origin-server-v1.4.0-rc1.b4e0954-linux-64bit.tar.gz
+    mv openshift-origin-server-v1.4.0-rc1+b4e0954-linux-64bit openshift
+fi
+
 nohup openshift/openshift start &
 sleep 5
 openshift/oc login https://localhost:8443 -u admin -p admin
