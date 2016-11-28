994b755a36c84b14c8b76e841e77324faae0a320
Henryk Konsek <hekonsek@gmail.com>
2016-11-28 12:15:01 +0100

    Fixed Docker configuration of chaos monkey.

--- distribution/chaosmonkey/src/main/groovy/net/streamok/distribution/chaosmonkey/ChaosMonkey.groovy
+++ distribution/chaosmonkey/src/main/groovy/net/streamok/distribution/chaosmonkey/ChaosMonkey.groovy
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat
 
 class ChaosMonkey {
 
-    Vertx vertx
+    private final Vertx vertx
 
     ChaosMonkey(Vertx vertx) {
         this.vertx = vertx
