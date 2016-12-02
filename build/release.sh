36e2dc6028d8d913cfde1def943c4f31f9ef4761
Henryk Konsek <hekonsek@gmail.com>
2016-12-02 18:26:55 +0100

    Upgraded Streamok version.

--- build/release.sh
+++ build/release.sh
@@ -30,6 +30,6 @@ fi
 mvn -Darguments='-Dmaven.test.skip=true' release:prepare release:perform
 
 
-sed -i -e "s/${OLD_VERSION}/${NEW_VERSION}/g" readme.md
+sed -i -e "s/${OLD_VERSION}/${NEW_VERSION}/g" readme.md distribution/install.sh
 
 mvn clean install -Pdocker -DskipTests
