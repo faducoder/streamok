4164a8cda82bed81a7ffd42f978ca34267ad0517
Henryk Konsek <hekonsek@gmail.com>
2016-12-08 16:13:29 +0100

    Refactoring.

--- service/machinelearning/src/main/groovy/net/streamok/service/machinelearning/decision/TrainDecisionModel.groovy
+++ service/machinelearning/src/main/groovy/net/streamok/service/machinelearning/decision/TrainDecisionModel.groovy
@@ -1,14 +1,27 @@
+/**
+ * Licensed to the Streamok under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.streamok.service.machinelearning.decision
 
 import io.vertx.core.eventbus.DeliveryOptions
-import io.vertx.core.json.Json
-import net.streamok.fiber.node.api.OperationContext
 import net.streamok.fiber.node.api.OperationDefinition
 import net.streamok.fiber.node.api.OperationHandler
 import net.streamok.service.machinelearning.ModelCache
 import org.apache.commons.lang3.Validate
 import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.PipelineModel
 import org.apache.spark.ml.PipelineStage
 import org.apache.spark.ml.classification.DecisionTreeClassifier
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
@@ -16,6 +29,9 @@ import org.apache.spark.ml.feature.*
 import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.SparkSession
+
+import static io.vertx.core.json.Json.decodeValue
+import static io.vertx.core.json.Json.encode
 
 class TrainDecisionModel implements OperationDefinition {
 
@@ -26,15 +42,13 @@ class TrainDecisionModel implements OperationDefinition {
 
     @Override
     OperationHandler handler() {
-        new OperationHandler() {
-            @Override
-            void handle(OperationContext operation) {
-                def spark = operation.dependency(SparkSession)
-                def models = operation.dependency(ModelCache)
-                def input = operation.nonBlankHeader('input')
-
-                operation.vertx().eventBus().send('document.find', Json.encode([size: 2000]), new DeliveryOptions().addHeader('collection', 'training_decision_' + input)) {
-                    def datax = Json.decodeValue(it.result().body().toString(), DecisionFeatureVector[]).toList()
+        { operation ->
+            def spark = operation.dependency(SparkSession)
+            def models = operation.dependency(ModelCache)
+            def input = operation.nonBlankHeader('input')
+            operation.vertx().eventBus().send('document.find', encode([size: 2000]), new DeliveryOptions().addHeader('collection', 'training_decision_' + input)) {
+                if(it.succeeded()) {
+                    def datax = decodeValue(it.result().body().toString(), DecisionFeatureVector[]).toList()
                     Validate.notEmpty(datax, "Training data can't be empty.")
 
                     List<LabeledPoint> labeledPoints = datax.collect { it.toLabeledPoint() }
@@ -52,32 +66,25 @@ class TrainDecisionModel implements OperationDefinition {
                             .setMaxCategories(4) // features with > 4 distinct values are treated as continuous.
                             .fit(data);
 
-// Split the data into training and test sets (30% held out for testing).
                     double[] spl = [0.8, 0.2].toArray()
                     Dataset<Row>[] splits = data.randomSplit(spl);
                     Dataset<Row> trainingData = splits[0];
                     Dataset<Row> testData = splits[1];
 
-// Train a TrainDecisionModel model.
-                    DecisionTreeClassifier dt = new DecisionTreeClassifier()
+                    def decisionTree = new DecisionTreeClassifier()
                             .setLabelCol("indexedLabel")
                             .setFeaturesCol("indexedFeatures");
 
-// Convert indexed labels back to original labels.
                     IndexToString labelConverter = new IndexToString()
                             .setInputCol("prediction")
                             .setOutputCol("predictedLabel")
                             .setLabels(labelIndexer.labels());
 
 // Chain indexers and tree in a Pipeline.
-                    PipelineStage[] st = [labelIndexer, featureIndexer, dt, labelConverter].toArray()
+                    PipelineStage[] st = [labelIndexer, featureIndexer, decisionTree, labelConverter].toArray()
                     Pipeline pipeline = new Pipeline()
                             .setStages(st);
-
-// Train model. This also runs the indexers.
-                    PipelineModel model = pipeline.fit(trainingData);
-
-// Make predictions.
+                    def model = pipeline.fit(trainingData)
                     Dataset<Row> predictions = model.transform(testData);
 
 // Select example rows to display.
@@ -89,9 +96,10 @@ class TrainDecisionModel implements OperationDefinition {
                             .setPredictionCol("prediction")
                             .setMetricName("accuracy");
                     double accuracy = evaluator.evaluate(predictions);
-                    System.out.println("Test Error = " + (1.0 - accuracy));
                     models.updateModel(input, 'decision', model)
-                    operation.reply(Json.encode(accuracy))
+                    operation.reply(encode(accuracy))
+                } else {
+                    operation.fail(100, it.cause().message)
                 }
             }
         }
