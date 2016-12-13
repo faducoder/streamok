ca80aa01ea800acf5532670fb5a4f49687fb0859
Henryk Konsek <hekonsek@gmail.com>
2016-12-13 14:19:53 +0100

    Added CMD installer.

--- distribution/cmd/src/main/bash/install-cmd.sh
+++ distribution/cmd/src/main/bash/install-cmd.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-wget
+wget https://raw.githubusercontent.com/streamok/streamok/master/distribution/cmd/src/main/bash/cmd.sh -O /usr/bin/streamok > /dev/null 2>&1
+chmod +x /usr/bin/streamok
+echo 'Streamok client has been installed into /usr/bin/streamok .'
