e9768270f2dded410a36673f1d2d63e292a9c2f8
Henryk Konsek <hekonsek@gmail.com>
2016-12-13 13:35:58 +0100

    Cmd should download Java client.

--- distribution/cmd.sh
+++ distribution/cmd.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-# Install Docker
-which docker > /dev/null || (curl -sSL https://get.docker.com/ | sh && sudo systemctl enable docker.service && sudo systemctl start docker)
+STREAMOK_VERSION=0.0.4
+
+mkdir -p ~/.streamok/downloads
+
+wget http://search.maven.org/remotecontent?filepath=net/streamok/streamok-distribution-cmd/${STREAMOK_VERSION}/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar\
+ -O ~/.streamok/downloads/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar
+java -jar ~/.streamok/downloads/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar install
 
 wget -nc https://github.com/openshift/origin/releases/download/v1.4.0-rc1/openshift-origin-server-v1.4.0-rc1.b4e0954-linux-64bit.tar.gz
 if [ ! -d 'openshift' ]; then
