3613c9df47e2184c97754cffe94625500d35f12e
Henryk Konsek <hekonsek@gmail.com>
2016-12-14 12:42:06 +0100

    Refactoring.

--- lib/download/src/test/groovy/net/smolok/lib/download/spring/DownloadManagerTest.groovy
+++ lib/download/src/test/groovy/net/smolok/lib/download/spring/DownloadManagerTest.groovy
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Streamok under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.smolok.lib.download.spring
 
 import net.smolok.lib.download.DownloadManager
@@ -11,7 +27,9 @@ import static com.google.common.io.Files.createTempDir
 import static net.streamok.lib.common.Uuids.uuid
 import static org.assertj.core.api.Assertions.assertThat
 
-public class DownloadManagerTest {
+class DownloadManagerTest {
+
+    // Fixtures
 
     def downloadManager = new DownloadManager(new DefaultProcessManager(), createTempDir())
 
@@ -22,15 +40,15 @@ public class DownloadManagerTest {
         // When
         downloadManager.download(new BinaryCoordinates(
                 new URL("http://search.maven.org/remotecontent?filepath=org/wildfly/swarm/guava/1.0.0.Alpha8/guava-1.0.0.Alpha8.jar"),
-                "guava.jar"));
+                "guava.jar"))
 
         // Then
-        long guavaSize = downloadManager.downloadedFile("guava.jar").length();
-        assertThat(guavaSize).isGreaterThan(0L);
+        def guavaSize = downloadManager.downloadedFile("guava.jar").length()
+        assertThat(guavaSize).isGreaterThan(0L)
     }
 
     @Test
-    void shouldDecompressTarGz() throws MalformedURLException {
+    void shouldDecompressTarGz() {
         // When
         downloadManager.download(new BinaryCoordinates(
                 new File("src/test/compressedDirectory.tar.gz").toURI().toURL(),
@@ -48,7 +66,7 @@ public class DownloadManagerTest {
                 "invalidCompressionFormat.xyz", "invalidCompressionFormat"));
     }
 
-    @Test(expected = FileDownloadException.class)
+    @Test(expected = FileDownloadException)
     void shouldHandleInvalidHost() throws MalformedURLException {
         // When
         downloadManager.download(new BinaryCoordinates(new URL("http://smolok-" + uuid() + ".com"), uuid()));
