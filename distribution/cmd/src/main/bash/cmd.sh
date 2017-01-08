3e43ea764bf721184c62499e37f76056fea6b423
Henryk Konsek <hekonsek@gmail.com>
2017-01-08 21:18:58 +0100

    Added PaaS startup to CMD.

--- distribution/cmd/src/main/bash/cmd.sh
+++ distribution/cmd/src/main/bash/cmd.sh
@@ -4,43 +4,11 @@ STREAMOK_VERSION=0.0.4
 
 mkdir -p ~/.streamok/downloads
 
-which javax > /dev/null 2>&1 || yum install -y java
+which java > /dev/null 2>&1 || yum install -y java
 
 wget -nc http://search.maven.org/remotecontent?filepath=net/streamok/streamok-distribution-cmd/${STREAMOK_VERSION}/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar\
  -O ~/.streamok/downloads/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar
-java -jar ~/.streamok/downloads/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar install
-
-if [ ! -d 'openshift' ]; then
-    wget -nc https://github.com/openshift/origin/releases/download/v1.4.0-rc1/openshift-origin-server-v1.4.0-rc1.b4e0954-linux-64bit.tar.gz
-    tar xpf openshift-origin-server-v1.4.0-rc1.b4e0954-linux-64bit.tar.gz
-    mv openshift-origin-server-v1.4.0-rc1+b4e0954-linux-64bit openshift
-fi
-
-nohup openshift/openshift start &
-sleep 10
-
-
-echo 'export KUBECONFIG=/root/openshift.local.config/master/admin.kubeconfig' >> /root/.bashrc
-echo 'export CURL_CA_BUNDLE=/root/openshift.local.config/master/ca.crt' >> /root/.bashrc
-echo 'export OC_HOME=/root/openshift' >> /root/.bashrc
-echo 'export PATH=$PATH:$OC_HOME' >> /root/.bashrc
-
-. bash
-echo '.bashrc reloaded'
-
-chmod +r /root/openshift.local.config/master/admin.kubeconfig
-
-oc login -u system:admin
-oc new-project streamok
-
-oadm policy add-scc-to-user anyuid -z default
-oadm policy add-role-to-user admin admin -n streamok
+java -jar ~/.streamok/downloads/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar start
 
 oc new-app mongo
-oc new-app streamok/node -e XMX=512m
-
-# Configure and create router
-oadm policy add-scc-to-user hostnetwork system:serviceaccount:streamok:router
-oadm policy add-cluster-role-to-user system:router system:serviceaccount:streamok:router
-oadm router streamok-router --service-account=router
-oc expose service node --name=node-route --hostname=46.101.209.237
+oc new-app streamok/node:${STREAMOK_VERSION} -e XMX=512m
--- distribution/cmd/src/main/groovy/net/streamok/distribution/cmd/StreamokCmd.groovy
+++ distribution/cmd/src/main/groovy/net/streamok/distribution/cmd/StreamokCmd.groovy
@@ -1,10 +1,16 @@
 package net.streamok.distribution.cmd
 
+import net.streamok.lib.download.DownloadManager
+import net.streamok.lib.paas.OpenShiftPaas
+import net.streamok.lib.process.DefaultProcessManager
+
 class StreamokCmd {
 
     static void main(String... args) {
-        if(args.first() == 'install') {
+        if(args.first() == 'install' || args.first() == 'start') {
             new DockerInstall().execute()
+            println 'Starting OpenShift...'
+            new OpenShiftPaas(new DownloadManager(new DefaultProcessManager(), new File("/tmp/download")), new DefaultProcessManager()).init().start()
         }
     }
 
