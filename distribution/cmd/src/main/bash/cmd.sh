4fbfc544a40711b108ef98232ffccff0a5d8c40a
Henryk Konsek <hekonsek@gmail.com>
2017-01-08 21:42:30 +0100

    Moved PaaS commands to CMD.

--- distribution/cmd/src/main/bash/cmd.sh
+++ distribution/cmd/src/main/bash/cmd.sh
@@ -9,6 +9,3 @@ which java > /dev/null 2>&1 || yum install -y java
 wget -nc http://search.maven.org/remotecontent?filepath=net/streamok/streamok-distribution-cmd/${STREAMOK_VERSION}/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar\
  -O ~/.streamok/downloads/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar
 java -jar ~/.streamok/downloads/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar start
-
-oc new-app mongo
-oc new-app streamok/node:${STREAMOK_VERSION} -e XMX=512m
--- distribution/cmd/src/main/groovy/net/streamok/distribution/cmd/StreamokCmd.groovy
+++ distribution/cmd/src/main/groovy/net/streamok/distribution/cmd/StreamokCmd.groovy
@@ -7,10 +7,17 @@ import net.streamok.lib.process.DefaultProcessManager
 class StreamokCmd {
 
     static void main(String... args) {
+        def paas = new OpenShiftPaas(new DownloadManager(new DefaultProcessManager(), new File("/tmp/download")), new DefaultProcessManager()).init()
         if(args.first() == 'install' || args.first() == 'start') {
             new DockerInstall().execute()
+
             println 'Starting OpenShift...'
-            new OpenShiftPaas(new DownloadManager(new DefaultProcessManager(), new File("/tmp/download")), new DefaultProcessManager()).init().start()
+            paas.start()
+            println 'OpenShift started.'
+            paas.startService('mongo')
+            paas.startService('streamok/node:0.0.4 -e XMX=512m')
+        } else if(args.first() == 'reset') {
+            paas.reset()
         }
     }
 
