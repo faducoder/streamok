fd2cf6bebd7b1c6cca527c5830142c03561bc557
Henryk Konsek <hekonsek@gmail.com>
2017-01-09 08:56:03 +0100

    CMD installer now supports branding.

--- distribution/cmd/src/main/bash/install-cmd.sh
+++ distribution/cmd/src/main/bash/install-cmd.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-wget https://raw.githubusercontent.com/streamok/streamok/master/distribution/cmd/src/main/bash/cmd.sh -O /usr/bin/streamok > /dev/null 2>&1
-chmod +x /usr/bin/streamok
-echo 'Streamok client has been installed into /usr/bin/streamok .'
+if [ -z "${BRANDING}" ]; then
+  BRANDING='streamok'
+fi
+
+wget https://raw.githubusercontent.com/streamok/streamok/master/distribution/cmd/src/main/bash/cmd.sh -O /usr/bin/${BRANDING} > /dev/null 2>&1
+chmod +x /usr/bin/${BRANDING}
+echo "Installed ${BRANDING} client into /usr/bin/${BRANDING} ."
