01445cb85bb7d6ec68a590fc16bd3daf56d3e15c
ajurasz <arkadiusz.jurasz@gmail.com>
2017-01-09 22:51:58 +0100

    Improve test coverage

--- service/machinelearningrest/src/test/groovy/net/streamok/service/machinelearningrest/MachineLearningRestServiceTest.groovy
+++ service/machinelearningrest/src/test/groovy/net/streamok/service/machinelearningrest/MachineLearningRestServiceTest.groovy
@@ -16,9 +16,13 @@
  */
 package net.streamok.service.machinelearningrest
 
+import io.vertx.core.eventbus.DeliveryOptions
 import io.vertx.ext.unit.TestContext
 import io.vertx.ext.unit.junit.VertxUnitRunner
 import net.streamok.fiber.node.DefaultServicesNode
+import net.streamok.fiber.node.api.OperationContext
+import net.streamok.fiber.node.api.OperationDefinition
+import net.streamok.fiber.node.api.OperationHandler
 import net.streamok.lib.mongo.EmbeddedMongo
 import net.streamok.service.document.DocumentService
 import org.junit.Test
@@ -32,6 +36,7 @@ import static java.util.UUID.randomUUID
 import static java.util.concurrent.TimeUnit.SECONDS
 import static net.streamok.lib.vertx.EventBuses.headers
 import static net.streamok.service.document.operations.DocumentSave.documentSave
+import static net.streamok.service.machinelearningrest.operation.CreateLabelContent.machineLearningRestCreateLabelContent
 import static net.streamok.service.machinelearningrest.operation.GetLabelForContent.machineLearningRestGetLabelForContent
 import static org.assertj.core.api.Assertions.assertThat
 
@@ -71,7 +76,7 @@ class MachineLearningRestServiceTest {
     void shouldFindTopNLabelsForContent(TestContext context) {
         // Given
         def async = context.async()
-        def testDocument = [id: '1', text: 'foo bar', labels: [iot: 50, vr: 70, cats: 99]]
+        def testDocument = [id: '2', text: 'foo bar', labels: [iot: 50, vr: 70, cats: 99]]
         def semaphore = new CountDownLatch(1)
         bus.send(documentSave, encode(testDocument), headers(collection: "ml_content_text_${dataset}")) {
             semaphore.countDown()
@@ -79,7 +84,7 @@ class MachineLearningRestServiceTest {
         semaphore.await(5, SECONDS)
 
         // When
-        bus.send(machineLearningRestGetLabelForContent, null, headers([id: '1', collection: "${dataset}", top: "1"])) {
+        bus.send(machineLearningRestGetLabelForContent, null, headers([id: '2', collection: "${dataset}", top: "1"])) {
             def result = decodeValue(it.result().body().toString(), List)
             assertThat(result.size()).isEqualTo(1)
             assertThat(result).endsWith('cats')
@@ -87,4 +92,46 @@ class MachineLearningRestServiceTest {
         }
     }
 
+    @Test
+    void shouldCreateLabelsForContent(TestContext context) {
+        // Given
+        def async = context.async()
+        def innerBus = new DefaultServicesNode().
+                addFiber(new MockMachineLearningLabelTextContent()).
+                addSuite(new MachineLearningRestService()).
+                addSuite(new DocumentService()).
+                vertx().eventBus()
+        def testDocument = [text: 'foo bar']
+
+        // When
+        def semaphore = new CountDownLatch(1)
+        innerBus.send(machineLearningRestCreateLabelContent, encode(testDocument), headers(id: 3, collection: "${dataset}")) {
+            semaphore.countDown()
+        }
+        semaphore.await(5, SECONDS)
+
+        // Then
+        innerBus.consumer('test.address', {
+            assertThat(it.headers().get('id')).isEqualTo('3')
+            assertThat(it.headers().get('collection')).isEqualTo(dataset)
+            async.complete()
+        })
+    }
+
+    class MockMachineLearningLabelTextContent implements OperationDefinition {
+
+        @Override
+        String address() {
+            'machineLearning.labelTextContent'
+        }
+
+        @Override
+        OperationHandler handler() {
+            { OperationContext operation ->
+                def contentId = operation.nonBlankHeader('id')
+                def collection = operation.nonBlankHeader('collection')
+                operation.vertx().eventBus().send('test.address', null, new DeliveryOptions().addHeader('id', contentId).addHeader('collection', collection))
+            }
+        }
+    }
 }
