9d3a1d4458707c5a96d81b7b097ee623fcbb0d96
Henryk Konsek <hekonsek@gmail.com>
2017-01-11 12:58:08 +0100

    CMD should support snapshots usage.

--- distribution/cmd/src/main/bash/cmd.sh
+++ distribution/cmd/src/main/bash/cmd.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-STREAMOK_VERSION=0.0.5
+if [ -z "${STREAMOK_VERSION}" ]; then
+  STREAMOK_VERSION=0.0.5
+fi
 
 mkdir -p ~/.streamok/downloads
 
 which java > /dev/null 2>&1 || yum install -y java
 
-wget -nc http://search.maven.org/remotecontent?filepath=net/streamok/streamok-distribution-cmd/${STREAMOK_VERSION}/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar\
- -O ~/.streamok/downloads/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar
-java -jar ~/.streamok/downloads/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar start
+if [[ $STREAMOK_VERSION != *"SNAPSHOT"* ]]; then
+    wget -nc http://search.maven.org/remotecontent?filepath=net/streamok/streamok-distribution-cmd/${STREAMOK_VERSION}/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar\
+        -O ~/.streamok/downloads/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar
+else
+    wget "https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=net.streamok&a=streamok-distribution-cmd&v=${STREAMOK_VERSION}&c=fat" \
+        -O ~/.streamok/downloads/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar
+fi
+
+java -jar ~/.streamok/downloads/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar "$@"
