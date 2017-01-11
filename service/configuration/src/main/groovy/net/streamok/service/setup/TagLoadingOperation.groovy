a9e2fb7b122dca455127bc691243ed481a873220
Pawel Lorek <palorek@gmail.com>
2017-01-11 17:03:28 +0100

    Improvements for Tag loading
    - it waits for the data from twitter
    - it reads the tags from Mongo

--- service/configuration/src/main/groovy/net/streamok/service/setup/TagLoadingOperation.groovy
+++ service/configuration/src/main/groovy/net/streamok/service/setup/TagLoadingOperation.groovy
@@ -1,39 +1,52 @@
 package net.streamok.service.setup
 
+import io.vertx.core.Handler
 import net.streamok.fiber.node.api.OperationContext
 import net.streamok.fiber.node.api.OperationDefinition
 import net.streamok.fiber.node.api.OperationHandler
 
+import java.util.concurrent.CountDownLatch
+
+import static java.util.concurrent.TimeUnit.SECONDS
+
 class TagLoadingOperation implements OperationDefinition {
 
-    // TODO - load from Mongo (Document read all )
-    private final List<String> tags = ['food', 'health'] // TODO - need to HTML exscape ? what about spaces 'machine learning', 'paralel comutation'
     private final String DEFAULT_SOURCE = 'twitter'
-    private final String DEFAULT_COLLECTION = 'testing' // TODO, load from config or sth
+    private final String ML_DATASET_COLLECTION = 'testing' // TODO, load from config or sth
+    private final TAG_COLLECTION = 'tags'
+    private final MAX_TAGS_READ = 10000
+
     @Override
     String address() {
-        return 'loadTags'
+        'loadTags'
     }
 
     @Override
     OperationHandler handler() {
-        return { context ->
-            tags.collect { createTag(it, context) }
-            teachMachine(context)
+        { context ->
+            context.send('document.find', ['size': MAX_TAGS_READ], ['collection': TAG_COLLECTION], Object) { tags ->
+                def semaphore = new CountDownLatch(tags.size())
+                tags.collect { createTag(it['name'], context) {
+                    semaphore.countDown()
+                } }
+                semaphore.await(15, SECONDS)
+                teachMachine(context)
+            }
+            context.reply(null)
         }
     }
 
     private
 
-    def createTag(String tag, OperationContext context){
+    def createTag(String tag, OperationContext context, Handler<Object> callback){
         def header = [
                 'source': "$DEFAULT_SOURCE:$tag",
-                'collection': DEFAULT_COLLECTION
+                'collection': ML_DATASET_COLLECTION
         ]
-        context.send('machineLearning.ingestTrainingData', null, header)
+        context.send('machineLearning.ingestTrainingData', null, header, Object, callback)
     }
 
     def teachMachine(OperationContext context){
-        context.send('machineLearning.trainTextLabelModel', null, ['dataset': DEFAULT_COLLECTION])
+        context.send('machineLearning.trainTextLabelModel', null, ['dataset': ML_DATASET_COLLECTION])
     }
 }
