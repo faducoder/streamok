2c5e471d6b92bbdfc5a2b4cfc67a2dae3d2ca420
Henryk Konsek <hekonsek@gmail.com>
2017-01-12 11:08:59 +0100

    Lowered verbosity of the CMD.

--- distribution/cmd/src/main/bash/cmd.sh
+++ distribution/cmd/src/main/bash/cmd.sh
@@ -9,11 +9,14 @@ mkdir -p ~/.streamok/downloads
 which java > /dev/null 2>&1 || yum install -y java
 
 if [[ $STREAMOK_VERSION != *"SNAPSHOT"* ]]; then
-    wget -nc http://search.maven.org/remotecontent?filepath=net/streamok/streamok-distribution-cmd/${STREAMOK_VERSION}/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar\
+    wget -q -nc http://search.maven.org/remotecontent?filepath=net/streamok/streamok-distribution-cmd/${STREAMOK_VERSION}/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar\
         -O ~/.streamok/downloads/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar
 else
-    wget "https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=net.streamok&a=streamok-distribution-cmd&v=${STREAMOK_VERSION}&c=fat" \
+    echo 'Downloading the latest version of the Streamok client...'
+    wget -q "https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=net.streamok&a=streamok-distribution-cmd&v=${STREAMOK_VERSION}&c=fat" \
         -O ~/.streamok/downloads/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar
+    echo 'Done.'
+    echo
 fi
 
 java -jar ~/.streamok/downloads/streamok-distribution-cmd-${STREAMOK_VERSION}-fat.jar "$@"
